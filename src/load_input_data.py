b713599f985b5d1c575c5faf37b8926d34a22bbe
Hikmat Ullah <hikmat@ziphq.com>
2024-03-20 20:37:42 +0500

    code changes

--- src/load_input_data.py
+++ src/load_input_data.py
@@ -2,36 +2,41 @@ from __future__ import annotations
 
 import csvfile
 
-from src.mappings.mapper import KeysGroup, Map
+from src.eib_builder import BusinessProcessType
+from src.mappings.mapper import Map
 
 
 class LoadInputData:
     def __init__(self, data_path):
         self.data_path = data_path
 
+    @classmethod
+    def load_csv(cls, file_name: str) -> list[Map]:
+        """
+        loads csv file and converts it into a list of objects
+        e.g
+        [
+            {"key1": "{row1.col1}", "key2": "{row1.col1}"},
+            {"key1": "{row2.col1}", "key2": "{row2.col1}"},
+            ...
+        ]
+        """
+
+        return [Map(row) for row in csvfile.load(file_name)]
 
     @classmethod
-    def load_csv(cls,file_name: str)-> list[Map]:
+    def load_json(
+        cls,
+        json_string: str,
+        process_type: BusinessProcessType = BusinessProcessType.CHANGE_JOB,
+    ) -> None:
         """
-            loads csv file and converts it into a list of objects
-            e.g
-            [
-                {"key1": "{row1.col1}", "key2": "{row1.col1}"},
-                {"key1": "{row2.col1}", "key2": "{row2.col1}"},
-                ...
-            ]
-        """
-
-        return [Map(row, KeysGroup.JOB_CHANGE) for row in csvfile.load(file_name)]
-    @classmethod
-    def load_json(cls,json_string: str)-> None:
-        """
-            loads json file and converts it into a list of objects
-            e.g
-            [
-                {"key1": "{row1.col1}", "key2": "{row1.col1}"},
-                {"key1": "{row2.col1}", "key2": "{row2.col1}"},
-                ...
-            ]
+        loads json file and converts it into a list of objects
+        e.g
+        [
+            {"key1": "{row1.col1}", "key2": "{row1.col1}"},
+            {"key1": "{row2.col1}", "key2": "{row2.col1}"},
+            ...
+        ]
         """
         pass
